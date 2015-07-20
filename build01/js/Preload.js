6526a5d5fa760d0e64bf365441b9264f5c3a2c79
Shaia <shaia.bak@hotmail.com>
2015-07-19 17:27:14 -0700

    sprite stuff and movment

--- build01/js/Preload.js
+++ build01/js/Preload.js
@@ -4,7 +4,7 @@ Preload = {
     //load game assets
     this.load.tilemap('level1', 'assets/tileset/level1.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('gameTiles', 'assets/img/tileset-placeholder2.png');
-    this.load.image('player', 'assets/img/player.png');
+    this.load.spritesheet('player', 'assets/img/mega-player.png',  30, 30);
   },
   create: function() {
     this.state.start('Game');
