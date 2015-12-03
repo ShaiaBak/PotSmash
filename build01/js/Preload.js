c2fd1ceb46cc2ec387728aaffe2b0995c30f29f5
Shaia <shaia.bak@hotmail.com>
2015-12-02 20:11:56 -0800

    textture stufff...

--- build01/js/Preload.js
+++ build01/js/Preload.js
@@ -8,7 +8,9 @@ Preload = {
 		this.load.spritesheet('item', 'assets/img/player.png',  64, 64);
 		this.load.spritesheet('player', 'assets/img/char_spritesheet_walk-v01.png',  64, 64);
 		this.load.spritesheet('potSprite_1', 'assets/img/barrel_breaking_sprite-32x32.png', 32, 32);
+
 		// this.load.image('testpot', 'assets/img/1_barrel-32x32.png');
+		this.load.image('invisibleBlock', 'assets/img/trans-1x1.png');
 		this.load.image('compass', 'assets/img/compass_rose.png');
 		this.load.image('touch_segment', 'assets/img/touch_segment.png');
 		this.load.image('touch', 'assets/img/touch.png');
