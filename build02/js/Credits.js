8f0de121eefd89c9457eec0e8fe5bb0db89455b1
slushee <tylerpham.9@gmail.com>
2016-01-10 17:58:08 -0800

    Added sprites to ending

--- build02/js/Credits.js
+++ build02/js/Credits.js
@@ -1,11 +1,24 @@
 var creditsText;
 var logol
 var textOverlay;
+var playerEnd1;
+var playerEnd2;
 
 var Credits = {
 	create: function() {
 		game.stage.backgroundColor = '#000';
-		var creditsStyle = {font: "16px Courier", fill: "#ffffff", align: "center" };
+		creditsLayer1 = game.add.image(this.game.world.centerX, 620, 'creditsBG');
+		creditsLayer1.anchor.set(0.5, 1);
+		creditsLayer1.smoothed = false;
+
+		playerEnd1 = game.add.sprite(300,100,'characterEnd');
+		playerEnd1.animations.add('playerEnd1Idle', [0,1,2,3], 8, true);
+		game.world.bringToTop(playerEnd1);
+		playerEnd2 = game.add.sprite(250,50,'characterEnd2');
+		playerEnd2.animations.add('playerEnd2Idle', [0,1,2,3], 8, true);
+		game.world.bringToTop(playerEnd2);
+
+		var creditsStyle = {font: "16px Courier", fill: "#000000", align: "center" };
 
 		creditsContent = ["Credits\n\n\n\n\n\nArt\nNAMES...\nNAMES...\nNAMES...\n\n\n\n\n\nProgramming\nNAMES...\nNAMES...\n\n\n\n\n\nMusic\nNAMES...\nNAMES...\nNAMES...\n\n\n\n\n\n\nThank You for playing!"]
 
@@ -17,10 +30,20 @@ var Credits = {
 	
 		logo = game.add.image(game.world.centerX, game.world.height, "lanternLogo");
 		logo.anchor.set(0.5,0);
+
+
+
+
+
+
+
+
 	},
 
 	update: function() {
 		//Values may change due to fonts and content
+		playerEnd1.animations.play('playerEnd1Idle');
+		playerEnd2.animations.play('playerEnd2Idle');
 		if (creditsText.y > -creditsText.height + 125) {
 			creditsText.y -= 1.0;
 			if (creditsText.y < -425){
